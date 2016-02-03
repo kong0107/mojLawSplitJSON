72bb932706f8da6c1159ba6350880558b316b30f
Kong Kao <kong0107@gmail.com>
2016-02-03 12:27:54 +0800

    Update UI

--- scripts/ngController.js
+++ scripts/ngController.js
@@ -1,5 +1,8 @@
 angular
 .module('app', ['ngRoute'])
+/*.config(['$locationProvider', function($locationProvider) {
+	$locationProvider.html5Mode(true);
+}])*/
 .config(['$routeProvider', function($routeProvider) {
 	$routeProvider
 	.when('/', {
@@ -13,22 +16,44 @@ angular
 	.otherwise({redirectTo: '/'});
 }])
 .controller('main', ['$scope', '$http', function($scope, $http) {
-	$http.get('UpdateDate.txt').then(function(res) {
+	$http.get('UpdateDate.txt', {cache: true}).then(function(res) {
 		$scope.UpdateDate = res.data;
 	}, function(res) { console.error(res); });
-}])
-.controller('list', ['$scope', '$http', function($scope, $http) {
-	$scope.itemsPerPage = 10;
-	$scope.skip = '0';
-	$http.get('index.json').then(function(res) {
-		$scope.laws = res.data;
-	}, function(res) {
-		console.error(res);
+	$scope.$on('setTitle', function(event, args) {
+		$scope.title = args.title;
 	});
-	$scope.typeof = function(v){return typeof v;};
 }])
-.controller('law', ['$scope', '$routeParams',
-	function($scope, $routeParams) {
-		$scope.test = 'LAW';
+.controller('list', ['$scope', '$http', '$routeParams',
+	function($scope, $http, $routeParams) {
+		$scope.$emit('setTitle', {title: '法規查詢'});
+		$scope.itemsPerPage = 10;
+		$scope.q = $routeParams.search;
+		$scope.skip = '0';
+		$http.get('index.json').then(function(res) {
+			$scope.laws = res.data;
+		}, function(res) {
+			console.error(res);
+		});
+		$scope.pages = function() {
+			if(!$scope.filtered) return [];
+			var result = [];
+			for(var i = 0;
+				i < $scope.filtered.length;
+				i += $scope.itemsPerPage
+			) result.push(i);
+			return result;
+		};
 	}
-]);
+])
+.controller('law', ['$scope', '$http', '$routeParams',
+	function($scope, $http, $routeParams) {
+		$scope.PCode = $routeParams.PCode;
+		var PCode = $routeParams.PCode;
+		$http.get('FalVMingLing/' + PCode + '.json').then(function(res) {
+			$scope.law = res.data;
+			$scope.$emit('setTitle', {title: res.data.法規名稱});
+		}, function(res) {
+			$scope.error = res;
+		});
+	}
+])
