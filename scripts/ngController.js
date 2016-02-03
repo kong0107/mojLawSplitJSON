0cf2ff5959d0b3fa20b70ccae50ff54b5acb2c7a
Kong Kao <kong0107@gmail.com>
2016-02-03 13:26:28 +0800

    upgrade efficiency

--- scripts/ngController.js
+++ scripts/ngController.js
@@ -1,11 +1,12 @@
 angular
 .module('app', ['ngRoute'])
-/*.config(['$locationProvider', function($locationProvider) {
-	$locationProvider.html5Mode(true);
-}])*/
 .config(['$routeProvider', function($routeProvider) {
 	$routeProvider
 	.when('/', {
+		templateUrl: 'list',
+		controller: 'list'
+	})
+	.when('/search/:search', {
 		templateUrl: 'list',
 		controller: 'list'
 	})
@@ -16,24 +17,31 @@ angular
 	.otherwise({redirectTo: '/'});
 }])
 .controller('main', ['$scope', '$http', function($scope, $http) {
-	$http.get('UpdateDate.txt', {cache: true}).then(function(res) {
+	$http.get('UpdateDate.txt').then(function(res) {
 		$scope.UpdateDate = res.data;
 	}, function(res) { console.error(res); });
 	$scope.$on('setTitle', function(event, args) {
 		$scope.title = args.title;
 	});
 }])
-.controller('list', ['$scope', '$http', '$routeParams',
-	function($scope, $http, $routeParams) {
-		$scope.$emit('setTitle', {title: '法規查詢'});
+.controller('list', ['$scope', '$http', '$routeParams', '$location',
+	function($scope, $http, $routeParams, $location) {
 		$scope.itemsPerPage = 10;
-		$scope.q = $routeParams.search;
-		$scope.skip = '0';
-		$http.get('index.json').then(function(res) {
+		$scope.q = $routeParams.search || '';
+		$http.get('index.json', {cache: true}).then(function(res) {
 			$scope.laws = res.data;
 		}, function(res) {
 			console.error(res);
 		});
+		$scope.search = function() {
+			var title = '法規查詢';
+			var q = $scope.q.trim();
+			//$location.url(q ? ('/search/' + q) : '/'); ///< the input would lose focus
+			if(q) title += '「' + q + '」';
+			$scope.$emit('setTitle', {title: title});
+			$scope.skip = '0';
+		};
+		$scope.search();
 		$scope.pages = function() {
 			if(!$scope.filtered) return [];
 			var result = [];
