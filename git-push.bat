c7afca469899a4dbad5243881c06143e1755601e
kong0107 <kong0107@gmail.com>
2022-12-09 12:01:41 +0800

    UpdateDate 20221202

--- git-push.bat
+++ git-push.bat
@@ -1,28 +1,10 @@
 @echo off
 
-cd xml
-copy ../aliases.json .
-git add .
-git commit -m "UpdateDate 20221118"
-git tag 20221118
-git push
-git push --tags
-cd ..
-
-cd json
-copy ../aliases.json .
-git add .
-git commit -m "UpdateDate 20221118"
-git tag 20221118
-git push
-git push --tags
-cd ..
-
 cd json_split
 copy ../aliases.json .
 git add .
-git commit -m "UpdateDate 20221125"
-git tag 20221125_swagger
+git commit -m "UpdateDate 20221202"
+git tag 20221202_swagger
 git push
 git push --tags
 cd ..
@@ -30,8 +12,8 @@ cd ..
 cd json_arrange
 copy ../aliases.json .
 git add .
-git commit -m "UpdateDate 20221125"
-git tag 20221125_arrange
+git commit -m "UpdateDate 20221202"
+git tag 20221202_arrange
 git push
 git push --tags
 cd ..
