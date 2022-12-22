aacc981e071ed837d629c0585f3f6e11b77701b9
kong0107 <kong0107@gmail.com>
2022-12-22 12:01:21 +0800

    UpdateDate 20221209

--- git-push.bat
+++ git-push.bat
@@ -1,19 +1,19 @@
 @echo off
 
 cd xml
-copy ../aliases.json .
+copy /B ../aliases.json .
 git add .
-git commit -m "UpdateDate 20221202"
-git tag 20221202
+git commit -m "UpdateDate 20221209"
+git tag 20221209
 git push
 git push --tags
 cd ..
 
 cd json
-copy ../aliases.json .
+copy /B ../aliases.json .
 git add .
-git commit -m "UpdateDate 20221202"
-git tag 20221202
+git commit -m "UpdateDate 20221209"
+git tag 20221209
 git push
 git push --tags
 cd ..
